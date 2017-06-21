433ef201feada92fab4ed698ff24b760b559f9b6
Peeyush Singh <peeyushsrj@gmail.com>
2017-06-21 17:21:01 +0530

    Create browse-files.go

---
+++ browse-files.go
@@ -0,0 +1,16 @@
+//Browse files of type x (e.g. `.mp3`) from root directory path, which returns array of file paths and error if exist.
+func BrowseDirectoryX(x string, root string) ([]string, error) {
+	var arr []string
+	err := filepath.Walk(root, func(path string, f os.FileInfo, err error) error {
+		if !f.IsDir() {
+			if strings.HasSuffix(f.Name(), x) { //.mp3
+				arr = append(arr, path)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return arr, err
+	}
+	return arr, nil
+}

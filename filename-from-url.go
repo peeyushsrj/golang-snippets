9539235269a93236794b0033b638b24bb6411307
peeyushsrj <peeyushsrj@gmail.com>
2017-07-06 21:48:31 +0530

    Updated function

--- filename-from-url.go
+++ filename-from-url.go
@@ -6,14 +6,12 @@ import (
 	"path/filepath"
 )
 
-
 //FilenameFromUrl takes input as a escaped url & outputs filename from it (unescaped - normal one)
 func FilenameFromUrl(urlstr string) string {
 	u, err := url.Parse(urlstr)
 	if err != nil {
 		log.Fatal("Error due to parsing url: ", err)
 	}
- 
-	x, _ := url.PathUnescape(u.EscapedPath())
+	x, _ := url.QueryUnescape(u.EscapedPath())
 	return filepath.Base(x)
 }
